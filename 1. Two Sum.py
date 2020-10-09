d7842b7ba3f5fab828552153169022537a6ede94
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-09 22:29:18 +0600

    feat: Add unit tests to `Two Sum` problem

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -10,6 +10,8 @@
 
 import unittest
 
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
@@ -20,3 +22,22 @@ class Solution:
                 return [i, d[target - nums[i]]]
                 
         return []
+
+class UnitTestForSolution(unittest.TestCase):
+
+    def test_num_1(self):
+        input_list = [2, 7, 11, 15]
+        target = 9
+
+        result = Solution().twoSum(input_list, target)
+        self.assertEqual(result, [0, 1])
+
+    def test_num_2(self):
+        input_list = [3, 2, 4]
+        target = 6
+
+        result = Solution().twoSum(input_list, target)
+        self.assertEqual(result, [1, 2])
+
+if __name__ == '__main__':
+    unittest.main()
