67b790be0dadb5df3c7c3df770de07873b81c126
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-12 22:25:17 +0600

    docs: Add score info to problem #7

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -6,6 +6,10 @@
 # within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose 
 # of this problem, assume that your function returns 0 when the reversed 
 # integer overflows.
+#
+# Score: Runtime: 24 ms, faster than 96.73% of Python3 online submissions for Reverse Integer.
+# Memory Usage: 14.1 MB, less than 99.98% of Python3 online submissions for Reverse Integer.
+
 import unittest
 
 class Solution:
