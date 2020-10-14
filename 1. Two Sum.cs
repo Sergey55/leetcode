19d4471743354e35e9870aaf4d6d35626ca4257c
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-14 20:48:03 +0600

    docs: Fix misspring in comment

--- 1. Two Sum.cs
+++ 1. Two Sum.cs
@@ -1,4 +1,4 @@
-/*Solution fo problem #1 `Two Sum`
+/*Solution for problem #1 `Two Sum`
 
 Description: Given an array of integers nums and an integer target, 
 return indices of the two numbers such that they add up to target.
